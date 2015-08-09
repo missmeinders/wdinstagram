a7e12aef667b965b8f2a3173dbb73c128a2e8ff7
hews <p@hews.co>
2015-08-08 18:13:28 -0700

    Define Entry model

--- models/entry.js
+++ models/entry.js
@@ -1,3 +1,18 @@
 var mongoose = require('mongoose');
+var moment   = require('moment');
 
-// Entry model
+var formatDate = function(date_taken) {
+  return moment(date_taken).format('MMM D, YYYY');
+};
+
+var entrySchema = new mongoose.Schema({
+  author:     String,
+  date_taken: {
+    type:    Date,
+    default: Date.now,
+    get:     formatDate
+  },
+  photo_url:  String
+});
+
+module.exports = mongoose.model('Entry', entrySchema);
